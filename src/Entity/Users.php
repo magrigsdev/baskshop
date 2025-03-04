713c12a629af2c701feb5a51cc0d33ad21bbae6b
magrigsdev <magrigsdesign@gmail.com>
2025-03-04 03:10:05 +0100

    creation users entity

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -19,6 +19,27 @@ class Users
     #[ORM\Column(length: 255)]
     private ?string $lastName = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $Email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Password = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $address = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $City = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $PostalCode = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Country = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Roles = "customer";
+
     public function getId(): ?int
     {
         return $this->id;
@@ -44,6 +65,90 @@ class Users
     public function setLastName(string $lastName): static
     {
         $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): static
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->Password;
+    }
+
+    public function setPassword(string $Password): static
+    {
+        $this->Password = $Password;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): static
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->City;
+    }
+
+    public function setCity(string $City): static
+    {
+        $this->City = $City;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->PostalCode;
+    }
+
+    public function setPostalCode(?string $PostalCode): static
+    {
+        $this->PostalCode = $PostalCode;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->Country;
+    }
+
+    public function setCountry(string $Country): static
+    {
+        $this->Country = $Country;
+
+        return $this;
+    }
+
+    public function getRoles(): ?string
+    {
+        return $this->Roles;
+    }
+
+    public function setRoles(string $Roles): static
+    {
+        $this->Roles = $Roles;
 
         return $this;
     }
