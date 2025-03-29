df08687fce5f41f9c9e8e2ba1e8f75d8fab36aca
fridev <magrigsdesign@gmail.com>
2025-03-29 20:16:22 +0100

    basketsSave fix

--- src/Services/BasketsServices.php
+++ src/Services/BasketsServices.php
@@ -21,24 +21,27 @@ class BasketsServices
     public function saveBaskets(array $baskets): bool
     {
         if (empty($baskets)) {
-            throw new \Exception('No users to insert');
+            throw new \Exception('No baskets to insert');
         }
 
         foreach ($baskets as $basket) {
+            // Vérifiez que chaque élément de $baskets est un tableau
             if (!is_array($basket)) {
                 throw new \Exception('Invalid basket format: expected array, got '.gettype($basket));
             }
 
+            // Créez un nouvel objet Baskets et définissez ses propriétés
             $new_basket = (new Baskets())
-            ->setBrand($basket['brand'])
-            ->setColor($basket['color'])
-            ->setName($basket['name'])
-            ->setSize($basket['size'])
-            ->setPrice($basket['price']);
-
-            $this->entity_manager->persist($new_basket);
-            $this->entity_manager->flush();
+                ->setBrand($basket['brand'])
+                ->setColor($basket['color'])
+                ->setName($basket['name'])
+                ->setSize($basket['size'])
+                ->setPrice($basket['price']);
+            // Persistez l'objet dans la base de données    
         }
+        $this->entity_manager->persist($new_basket);
+        // Effectuez un flush après avoir persisté tous les objets
+        $this->entity_manager->flush();
 
         return true;
     }
